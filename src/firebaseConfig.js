5bceb7268a2772a95c0cd158f118db0bbfa14847
Kanchan Bansal <112881045+kanchanbansal@users.noreply.github.com>
2023-10-08 13:37:17 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -19,16 +19,6 @@ const firebaseConfig = {
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBjltDIVsKGFiY2rpJJ5GYVZ-4deXty8Z8",
-  authDomain: "weekdays-4-30-pm.firebaseapp.com",
-  projectId: "weekdays-4-30-pm",
-  storageBucket: "weekdays-4-30-pm.appspot.com",
-  messagingSenderId: "377088344122",
-  appId: "1:377088344122:web:f9153dc28ea7d76bfbb503",
-  measurementId: "G-CPFGLSJ3L9"
->>>>>>> 8b605dbb30fe4bc01c3ec60774d645c7a851d9fa
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
